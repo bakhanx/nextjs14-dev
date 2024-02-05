380e27b7516d3f661263fb349b780291cc0fd1ab
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-02-05 22:11:56 +0900

    연락 페이지

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact",
+};
+
+const Contact = () => {
+  return <div>Contact</div>;
+};
+
+export default Contact;
