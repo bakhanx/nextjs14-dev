c4180d8481f81b9d5fa0055d47b092ac2d7b6e65
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-02-05 22:11:31 +0900

    메인 홈 페이지

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+export default function Home() {
+  return (
+    <main className="">
+      <div>This is Home Page</div>
+    </main>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navigation from "@/components/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YM Light",
+  title: {
+    template: "%s | YM Light",
+    default: "YM Light",
+  },
   description: "Light Boutique Shop",
 };
 
@@ -16,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navigation />
+        <div className="pt-10 p-2">{children}</div>
+      </body>
     </html>
   );
 }
