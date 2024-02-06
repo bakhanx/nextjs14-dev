bb85e071e0f122cef3ad509745ad34f60796c38d
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-02-06 18:19:12 +0900

    상품 디테일 페이지

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,19 +1,48 @@
-import { Metadata } from "next";
+import { Metadata, ResolvedMetadata, ResolvingMetadata } from "next";
 import Head from "next/head";
+import { API_URL } from "../page";
+import { Suspense } from "react";
+import MovieInfo, { MovieType, getMovies } from "../components/movie-info";
+import MovieVideos from "../components/movie-videos";
 
 type Props = {
   params: {
-    id: String;
+    id: string;
   };
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: {
+    [key: string]: string | string[];
+  };
 };
 
-export const metadata: Metadata = {
-  title: "Product",
+export const generateMetadata = async ({
+  params,
+  searchParams,
+}: Props): Promise<Metadata> => {
+  const movie: MovieType = await getMovies();
+  return { title: String(searchParams.name) };
 };
 
-const ProductDetail = ({ params }: Props) => {
-  return <div>{params.id}</div>;
+export const getMovie = async (id: string) => {
+  return await fetch(`${API_URL}/${id}`).then((res) => res.json());
+};
+
+const ProductDetail = async ({ params, searchParams }: Props) => {
+  const movie = await getMovie(params.id);
+
+  return (
+    <div className="flex">
+      <Suspense fallback={<div>Loading...</div>}>
+        <MovieInfo />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <MovieVideos id={params.id} name={String(searchParams.name)} />
+      </Suspense>
+      <div>
+          <h1>{movie?.title}</h1>
+          <div>{movie?.overview}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
