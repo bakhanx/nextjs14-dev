c57e5255bbd5c2e192fb101a4ad289c841c0f91a
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-02-06 18:19:44 +0900

    UI 수정

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Navigation = () => {
   return (
-    <nav className="fixed w-full">
+    <nav className="fixed w-full bg-slate-50 opacity-80">
       <ul className="flex gap-x-2 p-2 border-b-2">
         <li>
           <Link href="/">Home</Link>
