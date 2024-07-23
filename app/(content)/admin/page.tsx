d09110072590569d49ac63893aa8221a6f375cfd
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-07-23 22:01:48 +0900

    person store

--- app/(content)/admin/page.tsx
+++ app/(content)/admin/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+"use client";
+
+import { usePersonStore } from "@/app/(state)/name";
+import React, { ChangeEvent } from "react";
 
 const Admin = () => {
+  const firstName = usePersonStore((state) => state.firstName);
+  const lastName = usePersonStore((state) => state.lastName);
+  const updateFirstName = usePersonStore((state) => state.updateFirstName);
+  const updateLastName = usePersonStore((state) => state.updateLastName);
   return (
     <div>
-      adim
+      <div className="">admin Page</div>
+      <div className="flex flex-col">
+        <label htmlFor="">
+          First Name
+          <input
+            type="text"
+            onBlur={(e) => updateFirstName(e.currentTarget.value)}
+            className="border-2"
+          />
+        </label>
+        <label htmlFor="">
+          Last Name
+          <input
+            type="text"
+            onBlur={(e) => updateLastName(e.currentTarget.value)}
+            className="border-2"
+          />
+        </label>
+        <div className="mt-4">
+          <div>First Name : {firstName}</div>
+          <div>Last Name : {lastName}</div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
