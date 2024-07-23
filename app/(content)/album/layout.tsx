ae6e4b93699d87f89e681159b084dc74b29b775c
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-07-23 22:02:05 +0900

    기본 레이아웃

---
+++ app/(content)/album/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from "react";
+
+const AlbumLayout = ({
+  children,
+  modal,
+}: {
+  children: ReactNode;
+  modal: ReactNode;
+}) => {
+  return <div>{children} {modal}</div>;
+};
+
+export default AlbumLayout;
