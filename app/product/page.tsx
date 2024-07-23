646161a7dd627fdf08a5bd11788d574b76c098f1
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-07-23 22:03:23 +0900

    장바구니정보 컴포넌트 분리

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,17 +1,20 @@
 import { Metadata } from "next";
 import MovieInfo from "./components/movie-info";
-
+import { useStore } from "../(state)/product";
+import CartInfo from "./components/cart-info";
 
 export const metadata: Metadata = {
   title: "Product",
 };
 
-
-
 const Product = async () => {
+  
+
   return (
     <div>
       <h1>Detail</h1>
+      
+      <CartInfo />
       <MovieInfo />
     </div>
   );
