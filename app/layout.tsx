d16aed568004a5eb78b8e7f412c7366165d5b3a2
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-07 20:45:40 +0900

    store provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import Navigation from "@/components/navigation";
+import { CounterStoreProvider } from "@/providers/counter-store-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navigation />
-        <div className="pt-10 p-2">{children}</div>
+        <CounterStoreProvider>
+          {/* <Navigation /> */}
+          <div className="p-1">{children}</div>
+        </CounterStoreProvider>
       </body>
     </html>
   );
