770b369a8dd095d55ac7fdc5abf489b988545441
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-15 22:28:39 +0900

    supabase

---
+++ app/(content)/chat/supabase/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Supabase = () => {
+  return (
+    <div>
+      <h1>Supabase</h1>
+      <p className="w-1/2">
+        Supabase는 오픈소스 Firebase 대안입니다. Postgres 데이터베이스, 인증,
+        인스턴트 API, Edge 기능, 실시간 구독, 스토리지 및 벡터 임베딩으로
+        프로젝트를 시작하세요.
+      </p>
+    </div>
+  );
+};
+
+export default Supabase;
