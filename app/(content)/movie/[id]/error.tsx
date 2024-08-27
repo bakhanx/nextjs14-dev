303a9a8ceda4f6c9517747c280013e5941f1cb96
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2024-08-27 19:47:41 +0900

    에러페이지

---
+++ app/(content)/movie/[id]/error.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+const ProductDetailError = () => {
+  return (
+    <div>
+      This Component was occured error.
+    </div>
+  )
+}
+
+export default ProductDetailError
